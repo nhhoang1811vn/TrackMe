5a0f9dfa0a444a0a5599d00257c560953f11353b
HoangNH <nhhoang1811vn@gmail.com>
2020-08-28 19:05:48 +0700

    init google map lib

--- app/src/main/java/com/ssteam/trackme/presentation/recording/RecordingFragment.kt
+++ app/src/main/java/com/ssteam/trackme/presentation/recording/RecordingFragment.kt
@@ -5,9 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.ssteam.trackme.R
 
-class RecordingFragment : Fragment() {
+class RecordingFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -19,8 +26,28 @@ class RecordingFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_recording, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
     companion object {
         @JvmStatic
         fun newInstance() = RecordingFragment()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(
+            MarkerOptions()
+            .position(sydney)
+            .title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
